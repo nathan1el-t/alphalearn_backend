485fa238df73c78848c1b557adafd11f8c6d1e80
Chevinzte <cr.calunsag.2024@computing.smu.edu.sg>
2026-02-05 17:33:32 +0800

    connected backend to database

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+// package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MainController {
-    @GetMapping("/")
-    public String home() {
-        return "skibidi gyat 67 tung tung tung sahur";
-    }
-}
+// @RestController
+// public class MainController {
+//     @GetMapping("/")
+//     public String home() {
+//         return "skibidi gyat 67 tung tung tung sahur";
+//     }
+// }
